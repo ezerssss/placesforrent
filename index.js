4c9c50228340d6a7215b75a042303b481eab46d0
ezerssss <magbanuaezra@gmail.com>
2024-05-25 23:30:22 +0800

    wip3

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import {
     collection,
     getDocs,
+    query,
+    where,
 } from 'https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js';
 import db from './firebase/db.js';
 
@@ -11,14 +13,35 @@ const placesContainer = document.getElementById('places');
 window.addEventListener('load', async () => {
     const places = [];
 
-    const snapshot = await getDocs(placesCollectionRef);
+    const q = query(placesCollectionRef, where('type', '==', 'dorm'));
+
+    const snapshot = await getDocs(q);
 
     snapshot.forEach((doc) => {
         places.push(doc.data());
     });
 
+    console.log(places);
+
+    placesContainer.innerHTML = '';
+
     places.forEach((place) => {
-        const aWrapper = document.createElement('a');
-        aWrapper.href = `/property/?id=`;
+        placesContainer.innerHTML += `
+            <a href='/property/?id=${place.id}'>
+                <article class='property'>
+                    <img alt='${place.images[0]} src='{${place.images[0]}}'/>
+
+                    <div class='property-info'>
+                        <div>
+                            <h2 class='league-spartan-bold'>${place.name}</h2>
+                            <p class='karla'>
+                                <i class="fa-solid fa-lg fa-location-pin"></i>
+                                ${place.address}
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            </a>
+        `;
     });
 });
